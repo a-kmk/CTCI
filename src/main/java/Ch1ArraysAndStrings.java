9449867e69cfcf7423998571387e5d7d65656115
a-kmck <annakmieciak12@gmail.com>
2021-05-26 21:00:54 -0400

    Started working on 1.4

--- src/main/java/Ch1ArraysAndStrings.java
+++ src/main/java/Ch1ArraysAndStrings.java
@@ -1,8 +1,36 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Ch1ArraysAndStrings {
     public static void main(String[] args){
 
+    }
+
+    /**
+     * Regular string append function, takes O(n^2) time new string copy is created on each concat
+     * @param words
+     * @return
+     */
+    String joinWords(String[] words){
+        String sentence="";
+        for(String w: words){
+            sentence = sentence+w;
+        }
+        return sentence;
+    }
+
+    /**
+     * Stringbuilder is more efficient, creates resizable array of strings and copies only when necessary
+     * @param words
+     * @return
+     */
+    String joinWordsSB(String[] words){
+        StringBuilder sentence = new StringBuilder();
+        for(String w: words){
+            sentence.append(w);
+        }
+        return sentence.toString();
     }
 
     /**
@@ -25,8 +53,6 @@ public class Ch1ArraysAndStrings {
                }
            }
            return true;
-
-           // Another way, split by index, if the resulting array is longer than 2 elements, it's not unique
        }
     }
 
@@ -48,7 +74,8 @@ public class Ch1ArraysAndStrings {
     }
 
     /**
-     * 1.2 checks if a string s is a permutation of string t
+     * 1.2 checks if a string s is a permutation of string t by first sorting then
+     * checking for equality
      * @param s
      * @param t
      * @return boolean, true if a permutation, false if not
@@ -66,7 +93,46 @@ public class Ch1ArraysAndStrings {
         }
     }
 
+    /**
+     * 1.3 change spaces into %20 to make the string URL safe
+     * @param str
+     * @return
+     */
+    public String urlIfy(String str){
+        str=str.replace(" ","%20");
+        return str;
+    }
 
+    /**
+     * 1.4 Write a function to check if a given string is a permutation of a palindrome
+     * @param s
+     * @return
+     */
+    public String PalindromePermutation(String s){
+        Map<Character,Integer> map = new HashMap<Character,Integer>();
+
+        //store in hashmap ordered by character
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if (map.containsKey(c)){
+                int val = map.get(c);
+                val++;
+                map.put(c,val);
+            }
+            else{
+                map.put(c,1);
+            }
+        }
+
+        //if even check if all values are 2
+            //permute in every possible way
+        //if odd check if all values are 2 except for 'middle' that is 1
+            //permute in every possible way
+
+        //to be continued
+
+        return map.toString();
+    }
 
 
 }
