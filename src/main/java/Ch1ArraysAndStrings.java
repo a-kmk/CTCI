05670d54fbed3f65be1a059a37c8344479237abe
a-kmck <annakmieciak12@gmail.com>
2021-08-04 10:19:24 -0400

    Added some linked list problems

--- src/main/java/Ch1ArraysAndStrings.java
+++ src/main/java/Ch1ArraysAndStrings.java
@@ -165,7 +165,7 @@ public class Ch1ArraysAndStrings {
 
                     }
                     //TODO: permute and mirror before returning
-
+                    permutation("", toPermute.toString());
 
                 }
                 return toPermute.toString();
@@ -180,5 +180,14 @@ public class Ch1ArraysAndStrings {
         }
 
         return map.toString();
+    }
+
+    private static void permutation(String prefix, String str){
+        int n = str.length();
+        if (n == 0) System.out.println(prefix);
+        else {
+            for (int i = 0; i < n; i++)
+                permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+        }
     }
 }
---
+++ src/main/java/Ch2LinkedLists.java
@@ -0,0 +1,101 @@
+import java.util.HashMap;
+public class Ch2LinkedLists {
+
+    public static void main(String[] args){
+        Node head = new Node(1);
+        head.appendToTail(3);
+        head.appendToTail(2);
+        head.appendToTail(2);
+        head.appendToTail(2);
+        head.appendToTail(3);
+        head.appendToTail(2);
+        head.appendToTail(4);
+
+        System.out.println(head);
+        removeDups(head);
+        System.out.println(head);
+
+    }
+
+    static class Node{
+        Node next = null;
+        int data;
+
+        public Node(int d){
+            data =d;
+        }
+
+        void appendToTail(int d){
+            Node end = new Node(d);
+            Node n = this;
+
+            while(n.next !=null){
+                n=n.next;
+            }
+
+            n.next = end;
+        }
+        Node deleteNode(Node head, int d){
+            if (head==null) return null;
+
+            Node n = head;
+            if (n.data==d) return head.next;
+
+            while (n.next!=null){
+                if (n.next.data==d){
+                    n.next = n.next.next;
+                    return head;
+                }
+                n=n.next;
+            }
+            return head;
+        }
+        @Override
+        public String toString() {
+            String returnString ="";
+            Node n = this;
+
+            if (n == null) returnString+="This list is empty";
+
+            else{
+                returnString+=n.data + " ";
+                while (n.next!=null){
+                    returnString+=n.next.data + " ";
+                    n=n.next;
+                }
+            }
+
+            return returnString;
+        }
+
+    }
+
+    //2.1 Write code to remove duplicates from an unsorted linked list
+    static void removeDups(Node head){
+        //create hashmap to keep track of occurences
+        HashMap<Integer,Integer> occurences = new HashMap<Integer, Integer>();
+        if(head!=null) {
+            occurences.put(head.data, 1);
+            Node prev = head;
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+                if (occurences.get(current.data) != null) {
+                    prev.next = current.next;
+                    current = prev;
+                }
+                else {
+                    occurences.put(current.data, 1);
+                    prev=prev.next;
+                }
+            }
+
+        }
+    }
+
+    //2.1 Follow-up, same problem but no buffer allowed
+    static void removeDupsNoBuffer(Node head){
+        //for each entry, run to the end of the list and check if there are duplicates, if so remove them
+    }
+
+}
